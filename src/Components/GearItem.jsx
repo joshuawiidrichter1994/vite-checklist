96d66af0b5a04ffd75d9b16624993a937fa27fe3
Joshua Richter <joshuawiidrichter@gmail.com>
2025-04-15 15:44:09 +0200

    Added checkbox html structure and initial styling

--- src/Components/GearItem.jsx
+++ src/Components/GearItem.jsx
@@ -4,7 +4,10 @@ function GearItem({ item }) {
   return (
     <div className="item-container">
       <div className="gear-item">{item.name}</div>
-      <div className="check-box"></div>
+      <div className="check-box">
+        <div className="checked"></div>
+        <div className="unchecked"></div>
+      </div>
     </div>
   );
 }
